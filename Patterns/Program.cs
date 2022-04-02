ed50ed44e29599e0886001e78b919eaea69b09b5
Pedro Campos <pedro.campos@numeria.com.br>
2022-04-02 01:59:31 -0300

    Adicionar arquivos de projeto.

---
+++ Patterns/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Buscando o controle remoto =====");
+
+            var controleRemotoInteligente = new ControleRemotoInteligente();
+
+            controleRemotoInteligente.On(new LigarLuz());
+
+            controleRemotoInteligente.On(new LigarTv());
+        }
+    }
+
+
+    public class ControleRemotoInteligente // invoker
+    {
+        public ControleRemotoInteligente()
+        {
+
+        }
+
+        public void On(ICommand command)
+        {
+            command.Execute();
+        }
+    }
+
+
+    public class LigarLuz : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Ligou a luz");
+        }
+    }
+
+    public class LigarTv : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Ligar a Tv");
+        }
+    }
+
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
